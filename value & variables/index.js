0f2ba79ca7838a4015440e5ccb0bc4cdf3d00234
Aditya Ubuntu <aditsahu98@gmail.com>
2021-12-21 13:28:20 +0530

    value & variable

---
+++ value & variables/index.js
@@ -0,0 +1,21 @@
+var myName = 'Aditya Sahu';
+var myAge = 23;
+
+// practice
+var _myName = 'Arpit Sahu'
+var _1my__Name = 'Aishwary'
+// var 1myName = 'Arin'
+var $myName = "thapa technical"
+
+console.log(myName)
+console.log(myAge) 
+console.log(_myName)
+console.log(_1my__Name)
+// console.log(1myName)
+console.log($myName)
+
+
+
+// Notes : 
+// 1)variable name can never starts with first character as number
+// 2) cannot use special character while declaring variable name
