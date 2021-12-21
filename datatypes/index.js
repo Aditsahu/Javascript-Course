71f538f2491cf87269000d00d89e06c83ac5db17
Aditya Ubuntu <aditsahu98@gmail.com>
2021-12-21 13:45:24 +0530

    datatype

---
+++ datatypes/index.js
@@ -0,0 +1,11 @@
+var myName = "Aditya Sahu";
+console.log(myName);
+console.log(typeof(myName))
+
+var myAge = 23;
+console.log(myAge)
+console.log(typeof(myAge))
+
+var iAmAditya = true;
+console.log(iAmAditya)
+console.log(typeof(iAmAditya))
