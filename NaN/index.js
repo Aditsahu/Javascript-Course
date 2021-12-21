69d189101a17c0db8bb84e918be2452b3cfee65b
Aditya Ubuntu <aditsahu98@gmail.com>
2021-12-21 16:55:59 +0530

    Not-A-Number

---
+++ NaN/index.js
@@ -0,0 +1,13 @@
+// What is NaN ? 
+// 1) NaN is a property of the global object.
+// 2) In other words it is a variable in global scope
+// 3) The initial value of NaN is Not-A-Number.
+
+
+var myPhoneNum = 9876543210;
+var myName = 'Aditya'
+
+if(isNaN(myName))
+{
+    console.log('please enter valid phone number...')
+}
