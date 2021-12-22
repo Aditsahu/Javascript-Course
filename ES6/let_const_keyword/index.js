2930543a21ad10466a70bf5575578d55a41b2356
Aditya Ubuntu <aditsahu98@gmail.com>
2021-12-22 10:40:54 +0530

    removed variable & console statement

--- ES6/let_const_keyword/index.js
+++ ES6/let_const_keyword/index.js
@@ -3,13 +3,6 @@
 // var => Functions scope
 // let and const -> Block scope
 
-// var myName = "Aditya"
-// console.log(myName)
-
-// myName = "sahu"
-// console.log(myName)
-
-
 function biodata() {
     let myFirstName = "Aditya";
     console.log(myFirstName)
