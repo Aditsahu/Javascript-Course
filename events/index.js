fa1c1b728aab66fccd09199b673793fe36658d09
Aditya Ubuntu <aditsahu98@gmail.com>
2021-12-23 20:26:06 +0530

    Events in javascript

---
+++ events/index.js
@@ -0,0 +1,21 @@
+// Event in javascript 
+
+// HTML events are "things" that happen to HTML elements.
+// When javascript is used in HTML pages, Javascript can "react" on these events.
+
+// HTML Events
+// An HTML event can be something the browser does, or something a user does
+
+// Here are some examples of HTML Events :
+// An HTML web page has finished loading
+// An HTML input field was changed
+// An HTML Button was clicked
+// Often, when event happens, you may want to do something
+
+// Javascript lets you execute code when events are detected.
+
+// 4 ways of writing events in Javascript:
+// 1) Using inline event alert()
+// 2) By calling a function (We already seem and most common way of writing)
+// 3) Using Inline events (HTML onclick="" property and element.onclick)
+// 4) Using Event Listeners (addEventListener and IE's attachEvent)
